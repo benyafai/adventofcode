9ea1c5ab9d3897a9e4653635bd0ace7dd96eb7cc
Ben Yafai <ben@yaf.ai>
2023-12-08 01:02:22 +0000

    ✨ Day Seven

---
+++ 2023/07/part1.php
@@ -0,0 +1,97 @@
+<?php
+echo "<pre>";
+
+$games = [];
+$handle = fopen("./input.txt", "r");
+if ($handle) {
+    while (($line = fgets($handle)) !== false) {
+        // Typically, we'll parse our data out with regex
+        preg_match('/(\w{5}) (\d+)/', $line, $filtered);
+        // And add them to an array
+        $games[] = [
+            "hand" => $filtered[1],
+            "bid" => $filtered[2],
+        ];
+    }
+    fclose($handle);
+}
+
+
+// Magical usort
+usort($games, function($a, $b) {
+    // Card Values
+    $cards = [
+        "2" => 2, "3" => 3, "4" => 4, "5" => 5, "6" => 6, "7" => 7, "8" => 8,
+        "9" => 9, "T" => 10, "J" => 11, "Q" => 12, "K" => 13, "A" => 14,
+    ];
+    
+    // Rank our games, give them a fixed value
+    $hands = [];
+    foreach([$a, $b] as $id => $play) {
+        // Make life easier by converting all cards to number values
+        $splitHand = str_split($play["hand"]);
+        foreach ($splitHand as $c => $card) {
+            $splitHand[$c] = $cards[$card];
+        }
+        
+        // This is the special part
+        $count = array_count_values($splitHand);
+        arsort($count, SORT_NUMERIC);
+        
+        if (count($count) == 5) {
+            // 5 cards means all are different
+            $rank = 1; // High Card
+        } elseif (count($count) == 4) {
+            // 4 count means that only card is the same as another
+            $rank = 2; // One Pair 
+        } elseif (count($count) == 3) {
+            if($count[array_key_first($count)] == 2) {
+                $rank = 3; // Two Pair
+            } else {
+                $rank = 4; // Three of a kind
+            }
+        } elseif (count($count) == 2) {
+            if($count[array_key_first($count)] == 3) {
+                $rank = 5; // Full house (3 + 2
+            } else {
+                $rank = 6; // Four of a Kind
+            }
+        } elseif (count($count) == 1) {
+            // All cards are the same
+            $rank = 7; // Five of a kind
+        }
+        $hands[$id] = $play;
+        $hands[$id]["rank"] = $rank;
+        $hands[$id]["split"] = $splitHand;
+    }
+
+
+    // If the ranked value is the same, sort by Card value
+    if ($hands[0]["rank"] == $hands[1]["rank"]) {
+        for ($x = 0; $x <= 4; $x++) {
+            if ($hands[0]["split"][$x] > $hands[1]["split"][$x]) {
+                return 1;
+            } elseif ($hands[0]["split"][$x] < $hands[1]["split"][$x]) {
+                return -1;
+            }
+        }
+    }
+
+    // Assuming we have different ranks, the highest wins
+    if ($hands[0]["rank"] > $hands[1]["rank"]) {
+        return 1;
+    } elseif ($hands[0]["rank"] < $hands[1]["rank"]) {
+        return -1;
+    }
+    return 0;
+});
+
+// Now to calculate the winnings per bid
+foreach ($games as $rank => $game) {
+    $winnings[] = ($rank + 1) * $game["bid"];
+}
+
+
+$sum = array_sum($winnings);
+echo $sum;
+exit();
